5916eb992cdb047d41e3a719c1514367f6311093
Ang Zhen Yue <boborealmoon@gmail.com>
2024-03-07 21:00:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
 
 from flasgger import Swagger
 
-#constructor
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-#run
+# Fixed route function to correctly capture the input
+@app.route("/<name>")
+def routing(name):
+    return redirect(url_for("user", name=name))
+
+# New route to handle redirection
+@app.route("/user/<name>")
+def user(name):
+    return f"Welcome, {name}!"
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
