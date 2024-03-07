c44be386489f3f9e3583523e5353f303d2ea14bd
Ang Zhen Yue <boborealmoon@gmail.com>
2024-03-07 16:10:54 +0800

    calendar

--- calendar/calendar.py
+++ calendar/calendar.py
@@ -1,13 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask import request, jsonify
 from os import environ
-from flasgger import Swagger
 
-#constructor
 app = Flask(__name__)
 
-#conncecting to SQLAlchemy: 
-#The SQLAlchemy Database URI format is: dialect+driver://username:password@host:port/database
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -21,11 +18,6 @@ class Calendar(db.Model):
     TimeBegin = db.Column(db.Time, nullable=False)
     TimeEnd = db.Column(db.Time, nullable=False)
 
-    def __init__(self, TimeslotID, TimeBegin, TimeEnd):
-        self.TimeslotID = TimeslotID
-        self.TimeBegin = TimeBegin
-        self.TimeEnd = TimeEnd
-
     def json(self):
         return {
             "TimeslotID": self.TimeslotID,
@@ -33,25 +25,28 @@ class Calendar(db.Model):
             "TimeEnd": str(self.TimeEnd)
         }
 
+# @app.route("/displaycalendar")
+# def display():
+#     cal = calendar.TextCalendar(calendar.SUNDAY)
+#     month_calendar = cal.formatmonth(2024, 2)
+#     return (month_calendar)
+
+
 @app.route("/calendar")
 def getTimeslotID():
-    # Fetch all timeslots from the Calendar table
     calendar = Calendar.query.all()
 
     if calendar:
-        # Return a JSON response if calendar items exist
         timeslots = [{"TimeslotID": item.TimeslotID, "TimeBegin": str(item.TimeBegin), "TimeEnd": str(item.TimeEnd)} for item in calendar]
         return jsonify({
             "code": 200,
             "data": {"Timeslots": timeslots}
         })
     else:
-        # Return a 404 error if no calendar items are found
         return jsonify({
             "code": 404,
             "message": "No timeslots found."
         }), 404
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
