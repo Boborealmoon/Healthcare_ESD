f0c81b717cde8713fe2ad64acc92442790116736
JeremyCheongZhouMing <cheongzhouming@gmail.com>
2024-03-09 22:07:32 +0800

    copied place_order.py for reference

--- SubmitClaims/submit_claims.py
+++ SubmitClaims/submit_claims.py
@@ -16,3 +16,76 @@ claims_url = "http://localhost:5002/claims"
 # inventory_url = "http://localhost:5004/inventory"
 # order_url = "http://localhost:5005/order"
 patients_url = "http://localhost:5006/patient"
+
+@app.route("/place_order", methods=['POST'])
+def place_order():
+    # Simple check of input format and data of the request are JSON
+    if request.is_json:
+        try:
+            order = request.get_json()
+            print("\nReceived an order in JSON:", order)
+
+            # do the actual work
+            # 1. Send order info {cart items}
+            result = processPlaceOrder(order)
+            return jsonify(result), result["code"]
+
+        except Exception as e:
+            # Unexpected error in code
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            ex_str = str(e) + " at " + str(exc_type) + ": " + fname + ": line " + str(exc_tb.tb_lineno)
+            print(ex_str)
+
+            return jsonify({
+                "code": 500,
+                "message": "place_order.py internal error: " + ex_str
+            }), 500
+
+    # if reached here, not a JSON request.
+    return jsonify({
+        "code": 400,
+        "message": "Invalid JSON input: " + str(request.get_data())
+    }), 400
+
+
+def processPlaceOrder(order):
+    # 2. Send the order info {cart items}
+    # Invoke the order microservice
+
+    # 4. Record new order
+    # record the activity log anyway
+
+    # Check the order result; if a failure, send it to the error microservice.
+
+    # Inform the error microservice
+
+    # 7. Return error
+
+    # 5. Send new order to shipping
+    # Invoke the shipping record microservice
+
+    # Check the shipping result;
+    # if a failure, send it to the error microservice.
+
+    # Inform the error microservice
+
+    # 7. Return error
+
+    # 7. Return created order, shipping record
+    return {}
+
+
+# Execute this program if it is run as a main script (not by 'import')
+if __name__ == "__main__":
+    print("This is flask " + os.path.basename(__file__) +
+          " for placing an order...")
+    app.run(host="0.0.0.0", port=5100, debug=True)
+    # Notes for the parameters:
+    # - debug=True will reload the program automatically if a change is detected;
+    #   -- it in fact starts two instances of the same flask program,
+    #       and uses one of the instances to monitor the program changes;
+    # - host="0.0.0.0" allows the flask program to accept requests sent from any IP/host (in addition to localhost),
+    #   -- i.e., it gives permissions to hosts with any IP to access the flask program,
+    #   -- as long as the hosts can already reach the machine running the flask program along the network;
+    #   -- it doesn't mean to use http://0.0.0.0 to access the flask program.
