6e662150d5569df2a01029bf6f82d68aac0d7920
kevanscis <kevan.teo.2022@scis.smu.edu.sg>
2024-03-09 21:55:34 +0800

    Update book_appointment.py

--- book-appointment/book_appointment.py
+++ book-appointment/book_appointment.py
@@ -9,14 +9,10 @@ from invokes import invoke_http
 app = Flask(__name__)
 CORS(app)
 
-book_URL = "http://localhost:5000/book"
-order_URL = "http://localhost:5001/order"
-shipping_record_URL = "http://localhost:5002/shipping_record"
-activity_log_URL = "http://localhost:5003/activity_log"
-error_URL = "http://localhost:5004/error"
-
-
-appointment_URL="http://localhost:5001/appointments"
-calender_URL="http://localhost:5001/calendar"
-patient_URL="http://localhost:5001/patients"
-
+appointments_url = "http://localhost:5000/appointments"
+calendar_url = "http://localhost:5001/Clinic_calendar"
+claims_url = "http://localhost:5002/claims"
+employees_url = "http://localhost:5003/employee"
+inventory_url = "http://localhost:5004/inventory"
+order_url = "http://localhost:5005/order"
+patients_url = "http://localhost:5006/patient"
