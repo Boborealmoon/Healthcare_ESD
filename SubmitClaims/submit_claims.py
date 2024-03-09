b7d84802885a40b25ac9053c0b84ba86e765ebd8
JeremyCheongZhouMing <cheongzhouming@gmail.com>
2024-03-09 22:05:27 +0800

    created submit_claims.py

--- SubmitClaims/submit_claims.py
+++ SubmitClaims/submit_claims.py
@@ -10,9 +10,9 @@ app = Flask(__name__)
 CORS(app)
 
 appointments_url = "http://localhost:5000/appointments"
-calendar_url = "http://localhost:5001/Clinic_calendar"
+# calendar_url = "http://localhost:5001/Clinic_calendar"
 claims_url = "http://localhost:5002/claims"
-employees_url = "http://localhost:5003/employee"
-inventory_url = "http://localhost:5004/inventory"
-order_url = "http://localhost:5005/order"
+# employees_url = "http://localhost:5003/employee"
+# inventory_url = "http://localhost:5004/inventory"
+# order_url = "http://localhost:5005/order"
 patients_url = "http://localhost:5006/patient"
