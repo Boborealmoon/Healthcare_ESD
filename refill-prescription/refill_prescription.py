de7506cee0fea53ffdac4d4f5541b95770948f50
Kenneth Lim <kenneth.lim.2021@smu.edu.sg>
2024-03-14 23:17:53 +0800

    attempt 1

--- refill-prescription/refill_prescription.py
+++ refill-prescription/refill_prescription.py
@@ -12,126 +12,126 @@ inventory_url = "http://localhost:5004/inventory"
 order_url = "http://localhost:5005/order"
 
 
-@app.route("/place_order", methods=['POST'])
-def place_order():
-    # Simple check of input format and data of the request are JSON
-    if request.is_json:
-        try:
-            order = request.get_json()
-            print("\nReceived an order in JSON:", order)
+# @app.route("/place_order", methods=['POST'])
+# def place_order():
+#     # Simple check of input format and data of the request are JSON
+#     if request.is_json:
+#         try:
+#             order = request.get_json()
+#             print("\nReceived an order in JSON:", order)
 
-            # do the actual work
-            # 1. Send order info {cart items}
-            result = processPlaceOrder(order)
-            return jsonify(result), result["code"]
+#             # do the actual work
+#             # 1. Send order info {cart items}
+#             result = processPlaceOrder(order)
+#             return jsonify(result), result["code"]
 
-        except Exception as e:
-            # Unexpected error in code
-            exc_type, exc_obj, exc_tb = sys.exc_info()
-            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-            ex_str = str(e) + " at " + str(exc_type) + ": " + fname + ": line " + str(exc_tb.tb_lineno)
-            print(ex_str)
+#         except Exception as e:
+#             # Unexpected error in code
+#             exc_type, exc_obj, exc_tb = sys.exc_info()
+#             fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+#             ex_str = str(e) + " at " + str(exc_type) + ": " + fname + ": line " + str(exc_tb.tb_lineno)
+#             print(ex_str)
 
-            return jsonify({
-                "code": 500,
-                "message": "place_order.py internal error: " + ex_str
-            }), 500
+#             return jsonify({
+#                 "code": 500,
+#                 "message": "place_order.py internal error: " + ex_str
+#             }), 500
 
-    # if reached here, not a JSON request.
-    return jsonify({
-        "code": 400,
-        "message": "Invalid JSON input: " + str(request.get_data())
-    }), 400
-
-
-def processPlaceOrder(order):
-    # 2. Send the order info {cart items}
-    # Invoke the order microservice
-
-    print('\n-----Invoking order microservice-----')
-    order_result = invoke_http(order_URL, method='POST', json=order)
-    print('order_result:', order_result)
+#     # if reached here, not a JSON request.
+#     return jsonify({
+#         "code": 400,
+#         "message": "Invalid JSON input: " + str(request.get_data())
+#     }), 400
 
 
-    # 4. Record new order
-    # record the activity log anyway
+# def processPlaceOrder(order):
+#     # 2. Send the order info {cart items}
+#     # Invoke the order microservice
 
-    print('\n\n-----Invoking activity_log microservice-----')
-    invoke_http(activity_log_URL, method="POST", json=order_result)
-    print("\nOrder sent to activity log.\n")
-    # - reply from the invocation is not used;
-    # continue even if this invocation fails
-
-
-    # Check the order result; if a failure, send it to the error microservice.
-    code = order_result["code"]
-    if code not in range(200, 300):
+#     print('\n-----Invoking order microservice-----')
+#     order_result = invoke_http(order_URL, method='POST', json=order)
+#     print('order_result:', order_result)
 
 
-    # Inform the error microservice
-        print('\n\n-----Invoking error microservice as order fails-----')
-        invoke_http(error_URL, method="POST", json=order_result)
+#     # 4. Record new order
+#     # record the activity log anyway
 
-    # 7. Return error
+#     print('\n\n-----Invoking activity_log microservice-----')
+#     invoke_http(activity_log_URL, method="POST", json=order_result)
+#     print("\nOrder sent to activity log.\n")
+#     # - reply from the invocation is not used;
+#     # continue even if this invocation fails
+
+
+#     # Check the order result; if a failure, send it to the error microservice.
+#     code = order_result["code"]
+#     if code not in range(200, 300):
+
+
+#     # Inform the error microservice
+#         print('\n\n-----Invoking error microservice as order fails-----')
+#         invoke_http(error_URL, method="POST", json=order_result)
+
+#     # 7. Return error
         
-        return {
-            "code": 500,
-            "data": {"order_result": order_result},
-            "message": "Order creation failure sent for error handling."
-        }
+#         return {
+#             "code": 500,
+#             "data": {"order_result": order_result},
+#             "message": "Order creation failure sent for error handling."
+#         }
 
 
-    # 5. Send new order to shipping
-    # Invoke the shipping record microservice
+#     # 5. Send new order to shipping
+#     # Invoke the shipping record microservice
 
-    print('\n\n-----Invoking shipping_record microservice-----')
-    shipping_result = invoke_http(
-        shipping_record_URL, method="POST", json=order_result['data'])
-    print("shipping_result:", shipping_result, '\n')
-
-
-    # Check the shipping result;
-    # if a failure, send it to the error microservice.
-    code = shipping_result["code"]
-    if code not in range(200, 300):    
+#     print('\n\n-----Invoking shipping_record microservice-----')
+#     shipping_result = invoke_http(
+#         shipping_record_URL, method="POST", json=order_result['data'])
+#     print("shipping_result:", shipping_result, '\n')
 
 
-    # Inform the error microservice
+#     # Check the shipping result;
+#     # if a failure, send it to the error microservice.
+#     code = shipping_result["code"]
+#     if code not in range(200, 300):    
+
+
+#     # Inform the error microservice
         
-        print('\n\n-----Invoking error microservice as shipping fails-----')
-        invoke_http(error_URL, method="POST", json=shipping_result)
-        print("Shipping status ({:d}) sent to the error microservice:".format(
-            code), shipping_result)
+#         print('\n\n-----Invoking error microservice as shipping fails-----')
+#         invoke_http(error_URL, method="POST", json=shipping_result)
+#         print("Shipping status ({:d}) sent to the error microservice:".format(
+#             code), shipping_result)
 
         
 
-    # 7. Return error
-        return {
-            "code": 400,
-            "data": {
-                "order_result": order_result,
-                "shipping_result": shipping_result
-            },
-            "message": "Simulated shipping record error sent for error handling."
-        }
+#     # 7. Return error
+#         return {
+#             "code": 400,
+#             "data": {
+#                 "order_result": order_result,
+#                 "shipping_result": shipping_result
+#             },
+#             "message": "Simulated shipping record error sent for error handling."
+#         }
 
 
-    # 7. Return created order, shipping record
-    return {
-        "code": 201,
-        "data": {
-            "order_result": order_result,
-            "shipping_result": shipping_result
-        }
+#     # 7. Return created order, shipping record
+#     return {
+#         "code": 201,
+#         "data": {
+#             "order_result": order_result,
+#             "shipping_result": shipping_result
+#         }
 
-}
+# }
 
 
-# Execute this program if it is run as a main script (not by 'import')
-if __name__ == "__main__":
-    print("This is flask " + os.path.basename(__file__) +
-          " for placing an order...")
-    app.run(host="0.0.0.0", port=5100, debug=True)
+# # Execute this program if it is run as a main script (not by 'import')
+# if __name__ == "__main__":
+#     print("This is flask " + os.path.basename(__file__) +
+#           " for placing an order...")
+#     app.run(host="0.0.0.0", port=5100, debug=True)
 
     # Notes for the parameters:
     # - debug=True will reload the program automatically if a change is detected;
@@ -142,3 +142,40 @@ if __name__ == "__main__":
     #   -- as long as the hosts can already reach the machine running the flask program along the network;
     #   -- it doesn't mean to use http://0.0.0.0 to access the flask program.
 
+
+# -----------------
+@app.route("/refill_prescription")
+def check_inventory_threshold():
+    # Make a GET request to the /inventory endpoint to get all inventory items
+    response = requests.get("http://localhost:5004/inventory")
+    
+    # Check if the request was successful
+    if response.status_code in range(199,399):
+        inventory_data = response.json()["data"]["inventory"]
+        items_below_threshold = []
+
+        for item in inventory_data:
+            # Check if ProductQty is less than Threshold
+            if item["ProductQty"] < item["Threshold"]:
+                items_below_threshold.append({
+                    "ProductID" : item["ProductID"],
+                    "ProductName": item["ProductName"],
+                    "ProductQty": item["ProductQty"],
+                    "SupplierID": item["SupplierID"],
+                    "SupplierContactEmail": item["SupplierContactEmail"]
+                })
+
+        return items_below_threshold
+    else:
+        # Handle the case where the request was not successful
+        print("Failed to fetch inventory data")
+        return None
+
+if __name__ == "__main__":
+    items_below_threshold = check_inventory_threshold()
+    if items_below_threshold:
+        print("Items below threshold:")
+        for item in items_below_threshold:
+            print(item)
+    else:
+        print("No items found below threshold.")
