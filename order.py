1f443c01ed8c297c224300650d196a8b8d5d57e4
Ang Zhen Yue <boborealmoon@gmail.com>
2024-03-15 13:40:40 +0800

    compose up update

--- order.py
+++ order.py
@@ -4,7 +4,7 @@ from os import environ
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL', 'mysql+mysqlconnector://root@localhost:3306/orders')
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('dbURL', 'mysql+mysqlconnector://root:root@localhost:8889/orders')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -87,4 +87,4 @@ def create_order():
     return jsonify({'message': 'Order created successfully'}), 201
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5005, debug=True)
+    app.run(host="0.0.0.0", port=5005, debug=T
