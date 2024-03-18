f0bdc12c07fc6f50d34d1600a3f2521b36fddc19
JeremyCheongZhouMing <cheongzhouming@gmail.com>
2024-03-18 22:42:13 +0800

    Update patient.py

--- patient/patient.py
+++ patient/patient.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 #conncecting to SQLAlchemy: 
 #The SQLAlchemy Database URI format is: dialect+driver://username:password@host:port/database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/patients'
-# 'mysql+mysqlconnector://root:root@localhost:8889/claims'
+# 'mysql+mysqlconnector://root:root@localhost:8889/patients'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # export dbURL=mysql+mysqlconnector://root:root@localhost:8889/patients
