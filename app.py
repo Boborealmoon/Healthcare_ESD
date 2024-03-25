048fbf28a263da0569f5f09e59ffddb9252e2ce7
Ang Zhen Yue <boborealmoon@gmail.com>
2024-03-25 13:51:28 +0800

    testing

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ def index():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+# @app.route('/contact')
+# def contact():
+#     return render_template('contact.html')
 
 # Fixed route function to correctly capture the input
 # @app.route("/<name>")
